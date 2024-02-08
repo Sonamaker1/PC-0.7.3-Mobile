3e8d8ff6bb61c18b6d63fb5ea4c7c36c0a8c9b1d
Sonamaker1 <87080176+Sonamaker1@users.noreply.github.com>
2024-02-08 02:09:56 -0500

    Asset Clean
    Former-commit-id: 13ca1f8bdaccfe50d58af9cc4a01c9f6036a91d0 [formerly 7d641e85056e9d9f4f8578643581e1518b585752]
    Former-commit-id: ea79f8690d11c10fa4d7bb0ffc248a2b4a1ab163

--- assets/shared/stages/laboratory.lua
+++
@@ -1,6 +0,0 @@
-function onCreate()
-makeLuaSprite("laboratory","backgrounds/finnbg/laboratory",-150,-300)
-scaleObject('laboratory', 1.2, 1.2, true)
-addLuaSprite("laboratory",false)
-close(true)
-end
